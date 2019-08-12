9f753caf29ce28fa58c50c3cb3c93ac8531b48c1
Georgy Dzutsev <dzgeorgy@gmail.com>
2019-08-12 23:03:05 +0300

    Added algorithm for system calculation
    Signed-off-by: Georgy Dzutsev <dzgeorgy@gmail.com>

--- src/mainwindow/mainwindow.cpp
+++ src/mainwindow/mainwindow.cpp
@@ -3,9 +3,29 @@
 //
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <iostream>
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow) {
     ui->setupUi(this);
+    int i{0};
+    long double x{-0.1}, y{0.5};
+    long double matrix[2][2], dx, dy, result[2], norm;
+    do {
+        matrix[0][0] = der1_x(x, y);
+        matrix[0][1] = der1_y(x, y);
+        matrix[1][0] = der2_x(x, y);
+        matrix[1][1] = der2_y(x, y);
+        inverse_matrix(matrix);
+        dx = -matrix[0][0] * fun1(x, y) + -matrix[0][1] * fun2(x, y);
+        dy = -matrix[1][0] * fun1(x, y) + -matrix[1][1] * fun2(x, y);
+        x += dx;
+        y += dy;
+        result[0] = fun1(x, y);
+        result[1] = fun2(x, y);
+        norm = sqrt(result[0] * result[0] + result[1] * result[1]);
+        i++;
+        std::cout << "i = " << i << " x = " << x << " y = " << y << " dx = " << dx << " dy = " << dy << std::endl;
+    } while (norm >= 0.0000000000001);
 }
 
 MainWindow::~MainWindow() {
--- src/mainwindow/mainwindow.h
+++ src/mainwindow/mainwindow.h
@@ -4,18 +4,54 @@
 #pragma once
 
 #include <QMainWindow>
+#include <cmath>
 
 namespace Ui {
     class MainWindow;
 }
 
 class MainWindow : public QMainWindow {
-    Q_OBJECT
+Q_OBJECT
 
 public:
     explicit MainWindow(QWidget *parent = nullptr);
+
     ~MainWindow() override;
 
 private:
     Ui::MainWindow *ui;
+
+    static long double fun1(long double x, long double y) {
+        return sin(y + 0.5) - x - 1;
+    }
+
+    static long double fun2(long double x, long double y) {
+        return y + cos(x - 2);
+    }
+
+    static long double der1_x(long double x, long double y) {
+        return -1;
+    }
+
+    static long double der1_y(long double x, long double y) {
+        return cos(y + 0.5);
+    }
+
+    static long double der2_x(long double x, long double y) {
+        return -sin(x - 2);
+    }
+
+    static long double der2_y(long double x, long double y) {
+        return 1;
+    }
+
+    static void inverse_matrix(long double matrix[2][2]) {
+        long double det, element;
+        det = matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
+        element = matrix[1][1];
+        matrix[1][1] = matrix[0][0] / det;
+        matrix[0][0] = element / det;
+        matrix[0][1] = -matrix[0][1] / det;
+        matrix[1][0] = -matrix[1][0] / det;
+    }
 };
