99ddad32ba74a409fb70f08852cce919883ecb9c
Georgy Dzutsev <dzgeorgy@gmail.com>
2019-08-14 00:51:47 +0300

    Displays result of calculations
    Signed-off-by: Georgy Dzutsev <dzgeorgy@gmail.com>

--- src/mainwindow/mainwindow.cpp
+++ src/mainwindow/mainwindow.cpp
@@ -4,12 +4,13 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <iostream>
+#include <iomanip>
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow) {
     ui->setupUi(this);
     int i{0};
-    long double x{-0.1}, y{0.5};
-    long double matrix[2][2], dx, dy, result[2], norm;
+    double x{-0.1}, y{0.5}, matrix[2][2], dx, dy, result[2], norm;
+    std::string text;
     do {
         matrix[0][0] = der1_x(x, y);
         matrix[0][1] = der1_y(x, y);
@@ -24,8 +25,27 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWi
         result[1] = fun2(x, y);
         norm = sqrt(result[0] * result[0] + result[1] * result[1]);
         i++;
-        std::cout << "i = " << i << " x = " << x << " y = " << y << " dx = " << dx << " dy = " << dy << std::endl;
-    } while (norm >= 0.0000000000001);
+        text += "iteration = ";
+        text += std::to_string(i);
+        text += "\t x = ";
+        text += std::to_string(x);
+        text += "\t y = ";
+        text += std::to_string(y);
+        text += "\t dx = ";
+        text += std::to_string(dx);
+        text += "\t dy = ";
+        text += std::to_string(dy);
+        text += "\t norm = ";
+        text += std::to_string(norm);
+        text += "\n";
+        //std::cout << std::setprecision(8);
+        //std::cout << "i = " << i << " x = " << x << " y = " << y << " dx = " << dx << " dy = " << dy << " norm = " << norm << std::endl;
+    } while (norm >= 0.0001);
+    text += "Итоговый результат: \tx = ";
+    text += std::to_string(x);
+    text += "\t y = ";
+    text += std::to_string(y);
+    ui->label_4->setText(QString::fromStdString(text));
 }
 
 MainWindow::~MainWindow() {
--- src/mainwindow/mainwindow.h
+++ src/mainwindow/mainwindow.h
@@ -21,32 +21,32 @@ public:
 private:
     Ui::MainWindow *ui;
 
-    static long double fun1(long double x, long double y) {
+    static double fun1(double x, double y) {
         return sin(y + 0.5) - x - 1;
     }
 
-    static long double fun2(long double x, long double y) {
+    static double fun2(double x, double y) {
         return y + cos(x - 2);
     }
 
-    static long double der1_x(long double x, long double y) {
+    static double der1_x(double x, double y) {
         return -1;
     }
 
-    static long double der1_y(long double x, long double y) {
+    static double der1_y(double x, double y) {
         return cos(y + 0.5);
     }
 
-    static long double der2_x(long double x, long double y) {
+    static double der2_x(double x, double y) {
         return -sin(x - 2);
     }
 
-    static long double der2_y(long double x, long double y) {
+    static double der2_y(double x, double y) {
         return 1;
     }
 
-    static void inverse_matrix(long double matrix[2][2]) {
-        long double det, element;
+    static void inverse_matrix(double matrix[2][2]) {
+        double det, element;
         det = matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
         element = matrix[1][1];
         matrix[1][1] = matrix[0][0] / det;
